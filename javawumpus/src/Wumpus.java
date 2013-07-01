64a47572564ba04fd97e17b7a5690c0bc5f0ab1f
Shane Currier <shane.currier@mac.com>
2013-06-30 19:03:44 -0700

    Remove case/break Replace if/goto with normal if

--- javawumpus/src/Wumpus.java
+++ javawumpus/src/Wumpus.java
@@ -95,14 +95,19 @@ public class Wumpus {
 			case 800: ll = playerLocation(); break;																		// 800 l = l(1)
 			case 805: k = 1; break;																			// 805 for k = 1 to j9
 			case 810: break;
-			case 830: ll = isPathValidFromRoom(s[ll], p[k]) ? p[k] : s[ll][fnB()]; break;																// 830 l = s(l,fnb(1))
-			case 831: if (ll != mapItemLocations[2]) nextLine = 835; break;												// 900 if l <> l(2) then 920
-			case 832: println("AHA! YOU GOT THE WUMPUS!"); break;								// 905 print "AHA! YOU GOT THE WUMPUS!"
-			case 833: f = 1; break;																			// 910 f = 1
-			case 834: returnFromGosub(); break;																// 915 return
-			case 835: if (ll != playerLocation()) nextLine = 840; break;												// 920 if l <> l(1) then 840
-			case 836: println("OUCH! ARROW GOT YOU!"); break;									// 925 print "OUCH! ARROW GOT YOU!"
-			case 837: nextLine = 880; break;																// 930 goto 880
+			case 830: 
+				ll = isPathValidFromRoom(s[ll], p[k]) ? p[k] : s[ll][fnB()];																// 830 l = s(l,fnb(1))
+				if (ll == mapItemLocations[2]) {												// 900 if l <> l(2) then 920
+					println("AHA! YOU GOT THE WUMPUS!");								// 905 print "AHA! YOU GOT THE WUMPUS!"
+					f = 1;																			// 910 f = 1
+					returnFromGosub();
+				}
+																				// 915 return
+				if (ll == playerLocation()) {												// 920 if l <> l(1) then 840
+					println("OUCH! ARROW GOT YOU!");									// 925 print "OUCH! ARROW GOT YOU!"
+					nextLine = 880;																// 930 goto 880
+				}
+				break;
 			case 840: ++k; if (k <= j9) nextLine = 810; break;												// 840 next k
 			case 845: println("MISSED"); break;													// 845 print "MISSED"
 			case 850: ll = playerLocation(); break;																		// 850 l = l(1)
