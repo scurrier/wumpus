6487e98810a193efbac4076363ac12f73afdecb8
Shane Currier <shane.currier@mac.com>
2013-07-27 17:37:25 -0700

    Add PathsTest

---
+++ javawumpus/test/PathsTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PathsTest {
+
+	@Test
+	public void testCanGetToRoom() {
+		Paths testObj = new Paths(new int[]{0,1,2,3});
+		assertTrue(testObj.canGetToRoom(1));
+		assertTrue(testObj.canGetToRoom(2));
+		assertTrue(testObj.canGetToRoom(3));
+		assertFalse(testObj.canGetToRoom(4));
+	}
+
+}
