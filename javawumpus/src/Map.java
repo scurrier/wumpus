55a4e9bf37e22f863dcd8e74dd9b8c8857f8581a
Shane Currier <shane.currier@mac.com>
2013-07-27 17:38:25 -0700

    Get rid of extra data element that was used for zero indexing

--- javawumpus/src/Map.java
+++ javawumpus/src/Map.java
@@ -4,7 +4,7 @@ public class Map {
 	static Paths paths(int exit1, int exit2, int exit3) { return new Paths(new int[]{0,exit1,exit2,exit3}); }
 
 	public Map() {
-		this.s = new Paths[] {paths(0,0,0),
+		this.s = new Paths[] {
 				paths(2,5,8),		paths(1,3,10),		paths(2,4,12),		paths(3,5,14),		paths(1,4,6),
 				paths(5,7,15),		paths(6,8,17),		paths(1,7,9),		paths(8,10,18),		paths(2,9,11),
 				paths(10,12,19),	paths(3,11,13),		paths(12,14,20),	paths(4,13,15),		paths(6,14,16),
@@ -12,10 +12,10 @@ public class Map {
 	}
 
 	public boolean isValidMove(int startLocation, int endLocation) {
-		return s[startLocation].canGetToRoom(endLocation);
+		return s[startLocation-1].canGetToRoom(endLocation);
 	}
 
 	public Paths getRoomExits(int room) {
-		return s[room];
+		return s[room-1];
 	}
 }
