1a2b1d289e393a0eff16fce4807948c3796a01dc
Shane Currier <shane@enlearn.org>
2016-01-06 10:29:58 -0800

    Extract class Game from Program

--- CSharpWumpus/Wumpus/WumpusTest.cs
+++ CSharpWumpus/Wumpus/WumpusTest.cs
@@ -9,52 +9,52 @@ namespace Wumpus
         [SetUp]
         public void SetUp()
         {
-            Program.random = new Random(0); //seed random for expected results
+            Game.random = new Random(0); //seed random for expected results
         }
 
         [TestCase]
         public void fnA()
         {
-            Assert.AreEqual(15, Program.fnA());
-            Assert.AreEqual(17, Program.fnA());
-            Assert.AreEqual(16, Program.fnA());
-            Assert.AreEqual(12, Program.fnA());
-            Assert.AreEqual(5, Program.fnA());
-            Assert.AreEqual(12, Program.fnA());
-            Assert.AreEqual(19, Program.fnA());
-            Assert.AreEqual(9, Program.fnA());
-            Assert.AreEqual(20, Program.fnA());
-            Assert.AreEqual(6, Program.fnA());
+            Assert.AreEqual(15, Game.fnA());
+            Assert.AreEqual(17, Game.fnA());
+            Assert.AreEqual(16, Game.fnA());
+            Assert.AreEqual(12, Game.fnA());
+            Assert.AreEqual(5, Game.fnA());
+            Assert.AreEqual(12, Game.fnA());
+            Assert.AreEqual(19, Game.fnA());
+            Assert.AreEqual(9, Game.fnA());
+            Assert.AreEqual(20, Game.fnA());
+            Assert.AreEqual(6, Game.fnA());
         }
 
         [TestCase]
         public void fnB()
         {
-            Assert.AreEqual(3, Program.fnB());
-            Assert.AreEqual(3, Program.fnB());
-            Assert.AreEqual(3, Program.fnB());
-            Assert.AreEqual(2, Program.fnB());
-            Assert.AreEqual(1, Program.fnB());
-            Assert.AreEqual(2, Program.fnB());
-            Assert.AreEqual(3, Program.fnB());
-            Assert.AreEqual(2, Program.fnB());
-            Assert.AreEqual(3, Program.fnB());
-            Assert.AreEqual(1, Program.fnB());
+            Assert.AreEqual(3, Game.fnB());
+            Assert.AreEqual(3, Game.fnB());
+            Assert.AreEqual(3, Game.fnB());
+            Assert.AreEqual(2, Game.fnB());
+            Assert.AreEqual(1, Game.fnB());
+            Assert.AreEqual(2, Game.fnB());
+            Assert.AreEqual(3, Game.fnB());
+            Assert.AreEqual(2, Game.fnB());
+            Assert.AreEqual(3, Game.fnB());
+            Assert.AreEqual(1, Game.fnB());
         }
 
         [TestCase]
         public void fnC()
         {
-            Assert.AreEqual(3, Program.fnC());
-            Assert.AreEqual(4, Program.fnC());
-            Assert.AreEqual(4, Program.fnC());
-            Assert.AreEqual(3, Program.fnC());
-            Assert.AreEqual(1, Program.fnC());
-            Assert.AreEqual(3, Program.fnC());
-            Assert.AreEqual(4, Program.fnC());
-            Assert.AreEqual(2, Program.fnC());
-            Assert.AreEqual(4, Program.fnC());
-            Assert.AreEqual(2, Program.fnC());
+            Assert.AreEqual(3, Game.fnC());
+            Assert.AreEqual(4, Game.fnC());
+            Assert.AreEqual(4, Game.fnC());
+            Assert.AreEqual(3, Game.fnC());
+            Assert.AreEqual(1, Game.fnC());
+            Assert.AreEqual(3, Game.fnC());
+            Assert.AreEqual(4, Game.fnC());
+            Assert.AreEqual(2, Game.fnC());
+            Assert.AreEqual(4, Game.fnC());
+            Assert.AreEqual(2, Game.fnC());
         }
     }
 }
